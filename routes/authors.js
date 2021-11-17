ba75462bfd524cdd2c729d0a9392418086df8e6e
Austin Coffee <austincoffee@Austins-MacBook-Pro.local>
2021-11-16 22:06:12 -0800

    211116a

--- routes/authors.js
+++ routes/authors.js
@@ -1,20 +1,22 @@
 const express = require(`express`);
+const author = require("../models/author");
 const router = express.Router();
+const Author = require(`../models/author`);
 
 
 // All authors Route
 router.get(`/`, (req, res) => {
-    res.render(`index`);
+    res.render(`authors`);
 })
 
 // New Author Route
 router.get(`/new`, (req, res) => {
-    res.render(`authors/new`);
+    res.render(`authors/new`, { author: new Author() });
 })
 
 // Create Author Route
 router.post(`/`, (req, res) => {
-    res.send(`Create`);
+    res.send(req.body.name);
 })
 
 module.exports = router;
